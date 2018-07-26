c3e3612894008288689070f31fbd83a8cf633100
Baleean <ev.ball.20@gmail.com>
2018-07-27 00:48:43 +0300

    Файл настроен для подключения к базе данных

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=GB_Yii2',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
